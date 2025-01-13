4fc98b4a57ff0b2bb8b8335d5d239bfcabce4f2e
namezor90 <namezor90@gmail.com>
2025-01-13 15:27:25 +0100

    Update squirrel_analysis.py

--- squirrel_analysis.py
+++ squirrel_analysis.py
@@ -5,8 +5,8 @@ grey_squirrels_count = len(data[data["Primary Fur Color"] == "Gray"])
 cinnamon_squirrels_count = len(data[data["Primary Fur Color"] == "Cinnamon"])
 black_squirrels_count = len(data[data["Primary Fur Color"] == "Black"])
 data_dict = {
-    "students" : ["Gray", "Cinnamon", "Black"],
-    "scores" : [grey_squirrels_count, cinnamon_squirrels_count, black_squirrels_count],
+    "Fur Color" : ["Gray", "Cinnamon", "Black"],
+    "Count" : [grey_squirrels_count, cinnamon_squirrels_count, black_squirrels_count],
 }
 data = pandas.DataFrame(data_dict)
 data.to_csv('squirrels_color.csv')
